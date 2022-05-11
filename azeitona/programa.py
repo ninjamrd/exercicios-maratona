983cc2aedcc6a7ea6b7e36febdc87b7f6a5973b0
ninjamrd <90119765+ninjamrd@users.noreply.github.com>
2022-05-11 13:13:19 -0300

    aa
    aa

--- azeitona/programa.py
+++
@@ -1,6 +0,0 @@
-x=int(input())
-y=int(input())
-if(x <= 100 and x >= 1 and y <= 100 and y >= 1 ):
-    print(f"{x} {y}\n{y} {x}\n{y} {x-x*2}\n{x} {y-y*2}\n{x-x*2} {y-y*2}\n{y-y*2} {x-x*2}\n {y-y*2} {x}\n{x-x*2} {y}\n")
-else:
-    print("Error\n")
